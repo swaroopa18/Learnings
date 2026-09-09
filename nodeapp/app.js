8ab336c22a1450a9aa3ef9018fd6ce78d051c22a
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-09 14:35:34 +0530

    Due to asynchronous behaviour of node we should use the syncronous approach when we need to wait for response to complete before ending the request

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -14,14 +14,15 @@ const server = http.createServer((req, res) => {
         req.on("data", (chunk) => {
             body.push(chunk)
         })
-        req.on("end", () => {
+        return req.on("end", () => {
             const parseBody = Buffer.concat(body).toString();
             const message = parseBody.split("=")[1]
-            fs.writeFileSync("message.text", message)
+            fs.writeFile("message.text", message, () => {
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
         })
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
     }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
