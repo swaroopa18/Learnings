a02cf34e829515ce50e5981d60fd4ab934c41437
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-09 13:49:25 +0530

    Routing requests

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,8 +1,13 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.method, req.url, req.headers);
-
+    if (req.url === '/') {
+        res.write('<html>')
+        res.write('<div>hi</div>')
+        res.write('<body><form action="/message" method="post" ><input type="text" name="message"><button type"submit">submit</button></input></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<div>hi</div>')
