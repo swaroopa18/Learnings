5056b00ec161529c684581c82942efc481662cdf
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-09 13:39:47 +0530

    Addind response to server request

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.method, req.url, req.headers);
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<div>hi</div>')
+    res.write('<div>heloo</div>')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(3000)
