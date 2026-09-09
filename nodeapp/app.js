8d02b83173476c3b73bee57baf1653e61da29a87
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-09 13:19:44 +0530

    Add simple http server

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,4 +1,7 @@
-function sayHello(){
-    console.log("helloooo")
-}
-sayHello();
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+})
+
+server.listen(3000)
