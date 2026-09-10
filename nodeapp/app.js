5df1667a8c7d5cff0d2fd192d219426924bb49ff
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-10 15:11:30 +0530

    add package.json and express middlewares

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,6 +1,17 @@
 const http = require('http');
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes)
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("I am in middleware");
+    next(); // Allow the request to continue to next middleware
+})
+
+app.use((req, res, next) => {
+    console.log("I am in another middleware")
+})
+
+const server = http.createServer(app)
 
 server.listen(3000)
