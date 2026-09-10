25eac433aad81ebf8885844255b7866d046de573
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-10 17:38:37 +0530

    Parsing request bodies

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,17 +1,22 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("I am in middleware");
-    next(); // Allow the request to continue to next middleware
+app.use(bodyParser.urlencoded())
+
+app.use("/add-product", (req, res, next) => {
+    res.send("<body><form action='/product' method='post' ><input type='text' name='message'><button type'submit'>Add Product</button></input></form></body>")
 })
 
-app.use((req, res, next) => {
-    console.log("I am in another middleware")
+app.post("/product", (req, res) => {
+    console.log(req.body)
+    res.redirect("/")
+
 })
 
-const server = http.createServer(app)
+app.use("/", (req, res, next) => {
+    res.send("<h1>I am from expresss</h1>")
+})
 
-server.listen(3000)
+app.listen(3000)
