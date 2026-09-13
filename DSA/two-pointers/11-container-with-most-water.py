e293c65844bde713eae2c5ad4ad7c27f588ffb2b
Swaroopa Anisetti <swaroopareddy.iiit877@gmail.com>
2026-09-13 12:46:44 +0530

    Update contain with most water

--- DSA/two-pointers/11-container-with-most-water.py
+++ DSA/two-pointers/11-container-with-most-water.py
@@ -39,19 +39,71 @@ class Solution:
 # SC: O(1)    
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        maxarea = -math.inf
-        l, r = 0, len(height) - 1
+        n = len(height)
+        l, r = 0, n - 1
+
+        max_area = 0
+
+        # We need at least two different boundaries
+        # to form a container.
         while l < r:
-            width = r - l
+
+            # Store the current heights because we may move
+            # the pointers in the while loops below.
             l_height = height[l]
             r_height = height[r]
-            maxarea = max(maxarea, width * min(l_height, r_height))
 
-            if l_height < r_height:
+            # Calculate the area formed by the current
+            # left and right boundaries.
+            #
+            # The shorter boundary determines the amount
+            # of water that can be contained.
+            current_area = (r - l) * min(l_height, r_height)
+
+            # Keep the maximum area seen so far.
+            max_area = max(current_area, max_area)
+
+            # ------------------------------------------------
+            # If left boundary is shorter or equal
+            # ------------------------------------------------
+            if height[l] <= height[r]:
+
+                # The current container is limited by l_height.
+                #
+                # We are going to move l to the right, which
+                # decreases the width.
+                #
+                # Therefore, the only way to possibly get a
+                # larger area is to find a height GREATER than
+                # the current l_height.
+                #
+                # Any height <= l_height cannot help:
+                #
+                #     width decreases
+                #     height remains the same or decreases
+                #
+                # So we skip all such heights.
                 while l < r and height[l] <= l_height:
                     l += 1
+
+            # ------------------------------------------------
+            # If right boundary is shorter
+            # ------------------------------------------------
             else:
+
+                # The current container is limited by r_height.
+                #
+                # We are going to move r to the left, which
+                # decreases the width.
+                #
+                # Therefore, the only way to possibly get a
+                # larger area is to find a height GREATER than
+                # the current r_height.
+                #
+                # Any height <= r_height cannot help because
+                # the width becomes smaller and the limiting
+                # height does not increase.
                 while l < r and height[r] <= r_height:
                     r -= 1
-        return maxarea
 
+        return max_area
